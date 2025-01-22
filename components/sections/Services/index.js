501a535b346813815e3e4aa96d31ca3bb039f0f1
DevUniskai <development.uniskai@gmail.com>
2025-01-22 09:26:11 +0700

    - update captions

--- components/sections/Services/index.js
+++ components/sections/Services/index.js
@@ -24,42 +24,49 @@ const Services = () => {
         <ServiceItem
           src="/static/images/Fly.png"
           title="FLY"
-          description="Booking Flight Ticket"
+          // description="Booking Flight Ticket"
+          description="Elevate your journey with seamless flight bookings."
         />
         <ServiceItem
           src="/static/images/Stay.png"
           title="STAY"
-          description="Hotel Booking and Services"
+          // description="Hotel Booking and Services"
+          description="Indulge in exquisite stays at premier destinations."
         />
         <ServiceItem
           src="/static/images/passport.jpg"
           title="DOCS"
-          description="Passport, Visa and Travel Document"
+          // description="Passport, Visa and Travel Document"
+          description="Streamline your travel with effortless travel document services."
         />
         <ServiceItem
           src="/static/images/Tour.png"
           title="TOUR"
-          description="Tour Planning tailored to your needs giving the best travel experience"
+          // description="Tour Planning tailored to your needs giving the best travel experience"
+          description="Curated journeys crafted just for you."
         />
         <ServiceItem
           src="/static/images/SGX.png"
           title="SGX"
-          description="Self Travel with a brand new guided experience"
+          // description="Self Travel with a brand new guided experience"
+          description="Travel reimagined, with itineraries tailored to your preferences and our expertise."
         />
         <ServiceItem
           src="/static/images/Play.png"
           title="PLAY"
-          description="Book your attractions, concert, and rent cars"
-        />
-        <ServiceItem
-          src="/static/images/Docs.png"
-          title="B2B & CORPORATE"
-          description="Partner with us or enjoy exclusive perks for corporate customers"
+          // description="Book your attractions, concert, and rent cars"
+          description="Unlock extraordinary adventures and exclusive attractions."
         />
         <ServiceItem
           src="/static/images/cruise.png"
           title="CRUISE"
-          description="Partner with us or enjoy exclusive perks for corporate customers"
+          description="Set sail in ultimate luxury to unforgettable horizons."
+        />
+        <ServiceItem
+          src="/static/images/Docs.png"
+          title="B2B & CORPORATE"
+          // description="Partner with us or enjoy exclusive perks for corporate customers"
+          description="Partner in prestige, benefit from tailored corporate privileges."
         />
       </Grid>
     </Flex>
