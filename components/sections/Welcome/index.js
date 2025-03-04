129cba657a8ebf2d598c5b7252dae399a263d390
DevUniskai <development.uniskai@gmail.com>
2025-03-04 12:55:04 +0700

    - change to caution penipuan

--- components/sections/Welcome/index.js
+++ components/sections/Welcome/index.js
@@ -1,50 +1,59 @@
 import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 import Image from "next/image";
+import NextImage from "next/image";
 import NextLink from "next/link";
 import { Link } from "@chakra-ui/react";
 
 const Welcome = () => {
   return (
-    <Box bgColor={"Beige"}>
-      <Flex
-        height={"110vh"}
-        backgroundImage={"/static/images/bg.jpg"}
-        backgroundPosition={"center"}
+    // CODE LAMA ADA DI NOTION 
+    <Box bgColor="Beige" position="relative" height="100vh">
+      {/* Background layer */}
+      <Box
+        position="absolute"
+        top="0"
+        left="0"
+        width="100%"
+        height="100%"
+        backgroundImage="url('/static/images/waspada_bg.png')"
+        backgroundPosition="center"
         backgroundSize="cover"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-        color="Beige"
+        backgroundRepeat="no-repeat"
+        zIndex={1}
+      />
+
+      {/* Content layer */}
+      <Flex
+        position="relative"
+        zIndex={2}
+        height="100%"
+        align="center"
+        justify="center"
+        gap={8}
+        maxW="1600px"
+        mx="auto"
+        // px={4}
       >
-        <Image
-          src="/static/images/all-text.png"
-          alt="logo_uniskai_header"
-          width={1400}
-          height={100}
-        />
-
-        {/* <Heading size="3xl">Uniskai</Heading>
-        <Text size="lg" backgroundColor="lightBlue" borderRadius={5} p={1}>
-          Your Partner Travel Solution
-        </Text> */}
-      </Flex>
-
-      {/* SQTF HEADER */}
-
-      {/* <Box display="flex" justifyContent="center" height={"50hv"} mt={0.8}>
-        <Link
-          as={NextLink}
-          href={"/contact"}
-          _hover={{ textDecoration: "none" }}
-        >
-          <Image
-            src="/static/images/sqtf_gif.gif"
-            alt="example gif"
-            width="2000"
-            height="500"
+        {/* Phone image container */}
+        <Box position="relative" flex="1" height="100%" maxH="100%">
+          <NextImage
+            src="/static/images/waspada_phone-1.png"
+            alt="Waspada phone"
+            layout="fill"
+            objectFit="cover"
           />
-        </Link>
-      </Box> */}
+        </Box>
+
+        {/* Caution note container */}
+        <Box position="relative" flex="1" height="100%" maxH="100%">
+          <NextImage
+            src="/static/images/waspada_note-1.png"
+            alt="Waspada note"
+            layout="fill"
+            objectFit="contain"
+          />
+        </Box>
+      </Flex>
     </Box>
   );
 };
