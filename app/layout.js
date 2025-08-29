3368e01677969d4b72612d7195eee8d96d04dca9
DevUniskai <development.uniskai@gmail.com>
2025-08-29 14:12:28 +0700

    - add metadata

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,34 @@
+// import Navbar from "@/components/common/Navbar";
+// import "./globals.css";
+// import { Providers } from "./provider";
+
+// export const metadata = {
+//   title: "Uniskai",
+//   description: "Giving you the Best Travel Solution",
+// };
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <head>
+//         <meta name="agd-partner-manual-verification" />
+//         <title>Uniskai</title>
+//         <meta name="description" content="Giving you the Best Travel Solution" />
+//       </head>
+//       <body>
+//         <Providers>
+//           {/* <Navbar /> */}
+//           {children}
+//         </Providers>
+//       </body>
+//     </html>
+//   );
+// }
+
 import Navbar from "@/components/common/Navbar";
 import "./globals.css";
 import { Providers } from "./provider";
+import Script from "next/script";
 
 export const metadata = {
   title: "Uniskai",
@@ -14,6 +42,24 @@ export default function RootLayout({ children }) {
         <meta name="agd-partner-manual-verification" />
         <title>Uniskai</title>
         <meta name="description" content="Giving you the Best Travel Solution" />
+        
+        {/* Google Analytics */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script
+          id="ga-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+            `,
+          }}
+        />
       </head>
       <body>
         <Providers>
@@ -24,3 +70,4 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+
