2542e71b8aeb2eb73fc39fbcc06cb1438cc98c02
DevUniskai <development.uniskai@gmail.com>
2025-09-23 10:13:50 +0700

    - update back button - update contact list

--- components/common/BackButtonItem/index.js
+++ components/common/BackButtonItem/index.js
@@ -1,20 +1,26 @@
-"use client"
-import { Box, Button, Icon } from "@chakra-ui/react";
+"use client";
+import { Box, Button } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
+import { ArrowBackIcon } from "@chakra-ui/icons";
 import React from "react";
 
 const BackButton = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <Box >
-            <Button colorScheme="brown" onClick={router.back}>
-                Back
-            </Button>
-
-        </Box>
-        
-    );
+  return (
+    <Box>
+      <Button
+        onClick={() => router.back()}
+        leftIcon={<ArrowBackIcon />}
+        variant="ghost"
+        colorScheme="whiteAlpha" // kontras di bg cokelat
+        size="sm"
+        _hover={{ bg: "whiteAlpha.300" }}
+      >
+        Kembali
+      </Button>
+    </Box>
+  );
 };
 
 export default BackButton;
