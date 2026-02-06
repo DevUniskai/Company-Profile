256e68bb977a694b33f62c57f7a31931a9809d05
Deni <development.uniskai@gmail.com>
2026-02-06 10:35:02 +0700

    Add Temporary CNY 2026 blog

---
+++ app/blog/origami-angpao-2026/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Box, Heading } from "@chakra-ui/react";
+
+const CNY2026 = () => {
+  return (
+    <>
+      <Box
+        minHeight="100dvh"
+        bgColor="#AA381E"
+        alignContent="center"
+        justifyItems="center"
+      >
+        <Heading color="#ECBD00">Angpao, Folded Into Meaning.</Heading>
+      </Box>
+    </>
+  );
+};
+
+export default CNY2026;
