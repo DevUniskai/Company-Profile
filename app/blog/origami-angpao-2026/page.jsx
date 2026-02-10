26e302833bf1157073945dddf0a9fa77c76849ff
DevUniskai <development.uniskai@gmail.com>
2026-02-10 11:45:52 +0700

    add angpao tutorial

--- app/blog/origami-angpao-2026/page.jsx
+++ app/blog/origami-angpao-2026/page.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 
 export const metadata = {
   title: "CNY 2026",
@@ -12,10 +12,60 @@ const CNY2026 = () => {
       <Box
         minHeight="100dvh"
         bgColor="#AA381E"
-        alignContent="center"
-        justifyItems="center"
+        // alignContent="center"
+        // justifyItems="center"
+        textColor="#ECBD00"
       >
-        <Heading color="#ECBD00">Angpao, Folded Into Meaning.</Heading>
+        <Flex
+          flexDirection="column"
+          px={{ base: 10, sm: 16 }}
+          py={10}
+          maxW="800px"
+          mx="auto"
+          lineHeight="1.5"
+          // fontSize={{ base: "14px", sm: "16px", md: "18px" }}
+        >
+          <Heading as="h1" mt={6} mb={1}>
+            Angpao, Folded Into Meaning.
+          </Heading>
+          <Text mb={12}>
+            Some objects are meant to be used once. Others are meant to be
+            remembered.
+          </Text>
+
+          <Text>Angpao ini lahir dari sebuah pertanyaan sederhana:</Text>
+          <Text fontWeight="bold" mt={2}>
+            {/* <Text as="span" fontWeight="bold"> */}
+            bisakah sebuah amplop menjadi lebih dari sekadar wadah?
+            {/* </Text> */}
+          </Text>
+          <Text mt={2} mb={4}>
+            Bukan hanya menyimpan, tapi juga bercerita. Bukan hanya memberi,
+            tapi juga meninggalkan rasa.
+          </Text>
+
+          <Flex
+            backgroundPosition="center"
+            backgroundSize="cover"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            color="Beige"
+          >
+            <video
+              src="/static/images/angpao-step.mp4"
+              width="750"
+              height="200"
+              autoPlay
+              loop
+              muted
+              playsInline
+              controls
+              preload="auto"
+              style={{ objectFit: "cover" }}
+            />
+          </Flex>
+        </Flex>
       </Box>
     </>
   );
