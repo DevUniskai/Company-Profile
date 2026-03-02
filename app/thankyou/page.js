90a0032f01f5268e13f5516e1c9b311f11dcf618
Zev <zeviravm@gmail.com>
2026-03-02 09:26:58 +0700

    try push

--- app/thankyou/page.js
+++ app/thankyou/page.js
@@ -16,7 +16,7 @@ const ThankYouPage = () => {
       bg="Beige"
     >
       <Heading as="h1" mb={4} color={"brown"}>
-        Thank You for Booking Domestic Tour with Us!
+        Thank You for Booking Domestic Tour with Us~
       </Heading>
       <Text fontSize="lg" mb={8} color={"brown"}>
         Thank you for your reservation. 
