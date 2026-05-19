d019714761e04bb8bbbc57347f4308aba810004e
Zev <zeviravm@gmail.com>
2026-05-19 11:17:36 +0700

    Update: Remove Vercel.json & update in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,17 @@ const nextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/docs-upload-form",
+        destination: "https://script.google.com/macros/s/AKfycbySqIeM4yU09BcMNRHBdQHNugCvWqcerBq2oMrTannVv7GJySkS0dTqCB1PAXTW1sC0tg/exec",
+        permanent: true
+      }
+    ];
+  },
+
   async rewrites() {
     return TEMPLATE_SLUGS.flatMap((slug) => ([
       {
