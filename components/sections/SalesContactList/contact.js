048b3a96d6a6a539dddd2adacb4de24ecbcbe3f7
Deni <development.uniskai@gmail.com>
2026-07-22 10:13:00 +0700

    update prebook sqtf contact list

--- components/sections/SalesContactList/contact.js
+++ components/sections/SalesContactList/contact.js
@@ -1,17 +1,20 @@
 export const salesEvent = [
-  { name: "RONAV", phone: "6282295675189", bgColor: "rgba(239, 205, 214, 1)" },
-  { name: "DEVIN",
-    phone: "6282276384852", bgColor: "rgba(254, 240, 225, 1)" },
-  { name: "JANE", phone: "6285365200800", bgColor: "rgba(253, 248, 100, 1)" },
+  { name: "DEWI", phone: "6281283215788", bgColor: "rgba(239, 205, 214, 1)" },
+  { name: "ZEPHA", phone: "6285355765776", bgColor: "rgba(254, 240, 225, 1)" },
+  { name: "DINI", phone: "6282129904869", bgColor: "rgba(253, 248, 100, 1)" },
   {
-    name: "ZEPHA", phone: "6285355765776",
+    name: "FELICIA",
+    phone: "6282160024889",
     bgColor: "rgba(237, 177, 110, 0.3)",
   },
-  { name: "FELICIA",
-    phone: "6282160024889", bgColor: "rgba(254, 248, 91, 0.4)" },
   {
-    name: "SRI",
-    phone: "6285880573095",
+    name: "JANE",
+    phone: "6285365200800",
+    bgColor: "rgba(254, 248, 91, 0.4)",
+  },
+  {
+    name: "YASMINE",
+    phone: "6285207888388",
     bgColor: "rgba(237, 177, 110, 0.4)",
   },
   {
@@ -19,15 +22,19 @@ export const salesEvent = [
     phone: "628811906396",
     bgColor: "rgba(237, 177, 110, 0.4)",
   },
-  { name: "DENNIS", phone: "6285297580000", bgColor: "rgba(254, 248, 91, 0.9)" },
   {
-    name: "WENDY",
-    phone: "6282320008886",
+    name: "DENNIS",
+    phone: "6285297580000",
+    bgColor: "rgba(254, 248, 91, 0.9)",
+  },
+  {
+    name: "AUDREY",
+    phone: "6285207888877",
     bgColor: "rgba(237, 177, 110, 0.6)",
   },
   {
-    name: "YASMINE",
-    phone: "6285207888388",
+    name: "MALIK",
+    phone: "6281356681874",
     bgColor: "rgba(239, 205, 214, 1.2)",
   },
 ];
